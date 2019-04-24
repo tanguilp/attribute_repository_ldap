3b9b5f3dd9bc1f07afec49002386b8388adf33e0
Tangui <29804907+tanguilp@users.noreply.github.com>
2019-04-24 22:11:17 +0300

    Running format

--- lib/attribute_repository_ldap.ex
+++ lib/attribute_repository_ldap.ex
@@ -46,18 +46,17 @@ defmodule AttributeRepositoryLdap do
       end
 
     LDAPoolex.search(run_opts[:instance],
-              [
-                base: to_charlist(resource_id),
-                filter: :eldap.present('objectClass'),
-                attributes: attribute_list
-              ])
+      base: to_charlist(resource_id),
+      filter: :eldap.present('objectClass'),
+      attributes: attribute_list
+    )
     |> process_eldap_search_result(run_opts)
     |> case do
       [{_dn, attributes}] ->
         {:ok, attributes}
 
       [] ->
-        #FIXME: no param to except
+        # FIXME: no param to except
         {:error, AttributeRepository.Read.NotFoundError.exception("Entry not found")}
     end
   end
@@ -81,12 +80,11 @@ defmodule AttributeRepositoryLdap do
     IO.inspect(eldap_filter)
 
     LDAPoolex.search(run_opts[:instance],
-              [
-                base: to_charlist(run_opts[:base]),
-                scope: run_opts[:search_scope] || :eldap.singleLevel(),
-                filter: eldap_filter,
-                attributes: attribute_list
-              ])
+      base: to_charlist(run_opts[:base]),
+      scope: run_opts[:search_scope] || :eldap.singleLevel(),
+      filter: eldap_filter,
+      attributes: attribute_list
+    )
     |> case do
       {:eldap_search_result, _, _} = result ->
         process_eldap_search_result(result, run_opts)
@@ -104,19 +102,17 @@ defmodule AttributeRepositoryLdap do
   end
 
   defp build_eldap_filter({:and, lhs, rhs}) do
-    :eldap.and(
-      [
-        build_eldap_filter(lhs),
-        build_eldap_filter(rhs)
-      ])
+    :eldap.and([
+      build_eldap_filter(lhs),
+      build_eldap_filter(rhs)
+    ])
   end
 
   defp build_eldap_filter({:or, lhs, rhs}) do
-    :eldap.or(
-      [
-        build_eldap_filter(lhs),
-        build_eldap_filter(rhs)
-      ])
+    :eldap.or([
+      build_eldap_filter(lhs),
+      build_eldap_filter(rhs)
+    ])
   end
 
   defp build_eldap_filter({:not, filter}) do
@@ -125,111 +121,133 @@ defmodule AttributeRepositoryLdap do
     |> :eldap.not()
   end
 
-  defp build_eldap_filter({:pr, %AttributePath{
-    attribute: attribute,
-    sub_attribute: nil
-  }})
-  do
+  defp build_eldap_filter(
+         {:pr,
+          %AttributePath{
+            attribute: attribute,
+            sub_attribute: nil
+          }}
+       ) do
     attribute
     |> to_charlist()
     |> :eldap.present()
   end
 
-  defp build_eldap_filter({:eq, %AttributePath{
-    attribute: attribute,
-    sub_attribute: nil
-  }, value})
-  do
+  defp build_eldap_filter(
+         {:eq,
+          %AttributePath{
+            attribute: attribute,
+            sub_attribute: nil
+          }, value}
+       ) do
     attribute
     |> to_charlist()
     |> :eldap.equalityMatch(to_ldap_string_representation(value))
   end
 
-  defp build_eldap_filter({:ne, %AttributePath{
-    attribute: attribute,
-    sub_attribute: nil
-  }, value})
-  do
+  defp build_eldap_filter(
+         {:ne,
+          %AttributePath{
+            attribute: attribute,
+            sub_attribute: nil
+          }, value}
+       ) do
     attribute
     |> to_charlist()
     |> :eldap.equalityMatch(to_ldap_string_representation(value))
     |> :eldap.not()
   end
 
-  defp build_eldap_filter({:ge, %AttributePath{
-    attribute: attribute,
-    sub_attribute: nil
-  }, value})
-  do
+  defp build_eldap_filter(
+         {:ge,
+          %AttributePath{
+            attribute: attribute,
+            sub_attribute: nil
+          }, value}
+       ) do
     attribute
     |> to_charlist()
     |> :eldap.greaterOrEqual(to_charlist(value))
   end
 
-  defp build_eldap_filter({:le, %AttributePath{
-    attribute: attribute,
-    sub_attribute: nil
-  }, value})
-  do
+  defp build_eldap_filter(
+         {:le,
+          %AttributePath{
+            attribute: attribute,
+            sub_attribute: nil
+          }, value}
+       ) do
     attribute
     |> to_charlist()
     |> :eldap.lessOrEqual(to_ldap_string_representation(value))
   end
 
-  defp build_eldap_filter({:gt, %AttributePath{
-    attribute: attribute,
-    sub_attribute: nil
-  }, value})
-  do
+  defp build_eldap_filter(
+         {:gt,
+          %AttributePath{
+            attribute: attribute,
+            sub_attribute: nil
+          }, value}
+       ) do
     attribute
     |> to_charlist()
     |> :eldap.lessOrEqual(to_ldap_string_representation(value))
     |> :eldap.not()
   end
 
-  defp build_eldap_filter({:lt, %AttributePath{
-    attribute: attribute,
-    sub_attribute: nil
-  }, value})
-  do
+  defp build_eldap_filter(
+         {:lt,
+          %AttributePath{
+            attribute: attribute,
+            sub_attribute: nil
+          }, value}
+       ) do
     attribute
     |> to_charlist()
     |> :eldap.greaterOrEqual(to_ldap_string_representation(value))
     |> :eldap.not()
   end
 
-  defp build_eldap_filter({:co, %AttributePath{
-    attribute: attribute,
-    sub_attribute: nil
-  }, value})
-  do
+  defp build_eldap_filter(
+         {:co,
+          %AttributePath{
+            attribute: attribute,
+            sub_attribute: nil
+          }, value}
+       ) do
     attribute
     |> to_charlist()
-    |> :eldap.substrings([any: to_ldap_string_representation(value)])
+    |> :eldap.substrings(any: to_ldap_string_representation(value))
   end
 
-  defp build_eldap_filter({:sw, %AttributePath{
-    attribute: attribute,
-    sub_attribute: nil
-  }, value})
-  do
+  defp build_eldap_filter(
+         {:sw,
+          %AttributePath{
+            attribute: attribute,
+            sub_attribute: nil
+          }, value}
+       ) do
     attribute
     |> to_charlist()
-    |> :eldap.substrings([initial: to_charlist(value)])
+    |> :eldap.substrings(initial: to_charlist(value))
   end
 
-  defp build_eldap_filter({:ew, %AttributePath{
-    attribute: attribute,
-    sub_attribute: nil
-  }, value})
-  do
+  defp build_eldap_filter(
+         {:ew,
+          %AttributePath{
+            attribute: attribute,
+            sub_attribute: nil
+          }, value}
+       ) do
     attribute
     |> to_charlist()
-    |> :eldap.substrings([final: to_ldap_string_representation(value)])
+    |> :eldap.substrings(final: to_ldap_string_representation(value))
   end
 
-  @spec process_eldap_search_result({:eldap_search_result, list(), list()}, AttributeRepository.run_opts())
-  :: AttributeRepository.resource_list()
+  @spec process_eldap_search_result(
+          {:eldap_search_result, list(), list()},
+          AttributeRepository.run_opts()
+        ) :: AttributeRepository.resource_list()
 
   defp process_eldap_search_result({:eldap_search_result, result, _referrals}, run_opts) do
     for {:eldap_entry, dn, attributes} <- result do
@@ -247,19 +265,24 @@ defmodule AttributeRepositoryLdap do
     end
   end
 
-  @spec attribute_value_to_string(String.t(), charlist() | [charlist()], AttributeRepository.run_opts())
-  :: AttributeRepository.attribute_data_type()
+  @spec attribute_value_to_string(
+          String.t(),
+          charlist() | [charlist()],
+          AttributeRepository.run_opts()
+        ) :: AttributeRepository.attribute_data_type()
 
   defp attribute_value_to_string(attribute_name, attribute_value, run_opts) do
     case LDAPoolex.Schema.get(run_opts[:instance], to_string(attribute_name)) do
-      nil -> # no information from schema
+      # no information from schema
+      nil ->
         for single_value <- attribute_value do
           to_string(single_value)
         end
 
       %{single_valued: true, syntax: syntax} ->
         attribute_value
-        |> List.first() # theorically, it should be the only element
+        # theorically, it should be the only element
+        |> List.first()
         |> to_elixir_value(syntax)
 
       %{single_valued: false, syntax: syntax} ->
@@ -274,7 +297,10 @@ defmodule AttributeRepositoryLdap do
   defp to_elixir_value(value, @data_type_integer <> _), do: :string.to_integer(value) |> elem(0)
   defp to_elixir_value('TRUE', @data_type_boolean <> _), do: true
   defp to_elixir_value('FALSE', @data_type_boolean <> _), do: false
-  defp to_elixir_value(value, @data_type_generalized_time <> _), do: AttributeRepositoryLdap.Utils.DateParser.to_datetime(to_string(value))
+
+  defp to_elixir_value(value, @data_type_generalized_time <> _),
+    do: AttributeRepositoryLdap.Utils.DateParser.to_datetime(to_string(value))
+
   defp to_elixir_value(value, @data_type_octet_string <> _), do: {:binary_data, value}
   defp to_elixir_value(value, _), do: to_string(value)
 
@@ -283,7 +309,10 @@ defmodule AttributeRepositoryLdap do
   defp to_ldap_string_representation(val) when is_integer(val), do: to_charlist(val)
   defp to_ldap_string_representation(true), do: 'TRUE'
   defp to_ldap_string_representation(false), do: 'FALSE'
-  defp to_ldap_string_representation(%DateTime{} = val), do: DateTime.to_iso8601(val, :basic) |> String.replace("T", "") |> to_charlist()
+
+  defp to_ldap_string_representation(%DateTime{} = val),
+    do: DateTime.to_iso8601(val, :basic) |> String.replace("T", "") |> to_charlist()
+
   defp to_ldap_string_representation({:binary_data, bin}), do: to_charlist(bin)
   defp to_ldap_string_representation(nil), do: '\x00'
   defp to_ldap_string_representation(val), do: to_charlist(val)
--- lib/attribute_repository_ldap/utils/date_parser.ex
+++ lib/attribute_repository_ldap/utils/date_parser.ex
@@ -36,58 +36,59 @@ defmodule AttributeRepositoryLdap.Utils.DateParser do
   fraction =
     choice([
       string("."),
-      string(","),
+      string(",")
     ])
     |> ignore()
     |> ascii_string([?0..?9], min: 1)
     |> tag(:fraction)
 
   g_time_zone =
-    choice(
-      [
-        string("Z"),
-        choice([string("+"), string("-")])
-        |> ascii_string([?0..?9], 2)
-        |> optional(ascii_string([?0..?9], 2))
-      ]
-    )
+    choice([
+      string("Z"),
+      choice([string("+"), string("-")])
+      |> ascii_string([?0..?9], 2)
+      |> optional(ascii_string([?0..?9], 2))
+    ])
     |> tag(:timezone)
 
-  defparsec :datetime,
-  year
-  |> concat(month)
-  |> concat(day)
-  |> concat(hour)
-  |> optional(minute_second)
-  |> optional(fraction)
-  |> concat(g_time_zone)
+  defparsec(
+    :datetime,
+    year
+    |> concat(month)
+    |> concat(day)
+    |> concat(hour)
+    |> optional(minute_second)
+    |> optional(fraction)
+    |> concat(g_time_zone)
+  )
 
   def to_datetime(iso8601_basic) do
     {:ok, m, _, _, _, _} = datetime(iso8601_basic)
 
     iso8601_extended =
-      "#{m[:year]}"
-      <> "-"
-      <> "#{m[:month]}"
-      <> "-"
-      <> "#{m[:day]}"
-      <> "T"
-      <> "#{m[:hour]}"
-      <> ":"
-      <> "#{m[:minute] || 00}"
-      <> ":"
-      <> "#{m[:second] || 00}"
-      <> "."
-      <> "#{m[:fraction] || 0}"
-      <> case m[:timezone] do
-        ["Z"] ->
-          "Z"
+      "#{m[:year]}" <>
+        "-" <>
+        "#{m[:month]}" <>
+        "-" <>
+        "#{m[:day]}" <>
+        "T" <>
+        "#{m[:hour]}" <>
+        ":" <>
+        "#{m[:minute] || 00}" <>
+        ":" <>
+        "#{m[:second] || 00}" <>
+        "." <>
+        "#{m[:fraction] || 0}" <>
+        case m[:timezone] do
+          ["Z"] ->
+            "Z"
 
-        [sign, hour] ->
-          sign <> "#{hour}"
-        [sign, hour, minute] ->
-          sign <> "#{hour}" <> ":" <> "#{minute}"
-      end
+          [sign, hour] ->
+            sign <> "#{hour}"
+
+          [sign, hour, minute] ->
+            sign <> "#{hour}" <> ":" <> "#{minute}"
+        end
 
     DateTime.from_iso8601(iso8601_extended)
     |> elem(1)
